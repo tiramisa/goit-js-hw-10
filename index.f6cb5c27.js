b6844dfacfc2485b25b49901a05eefcdba099641
tiramisa <123115402+tiramisa@users.noreply.github.com>
2023-07-13 13:27:30 +0000

    Deploying to gh-pages from @ tiramisa/goit-js-hw-10@607706d880fc838752fe16b3dfff23359a78a091 🚀

--- index.f6cb5c27.js
+++
@@ -1,2 +0,0 @@
-var e=this;window.addEventListener("scroll",function(r){document.documentElement.style.setProperty("--scrollTop","".concat(e.scrollY,"px"))}),gsap.registerPlugin(ScrollTrigger,ScrollSmoother),ScrollSmoother.create({wrapper:".wrapper",content:".content"});
-//# sourceMappingURL=index.f6cb5c27.js.map
